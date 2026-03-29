7fca88554f7598cec8849ada04e8c29212ce48b4
Carlos Menco Guardo <84938811+carlosmencog@users.noreply.github.com>
2026-03-29 17:11:05 -0500

    adding support to blog collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,15 @@
 import { defineCollection, z } from 'astro:content';
 
+const blog = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    author: z.string().optional(),
+    thumbnail: z.string().optional(),
+    body: z.string().optional(),
+  }),
+});
+
 const pages = defineCollection({
   schema: z.object({
     title: z.string(),
