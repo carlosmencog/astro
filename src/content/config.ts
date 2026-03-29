1853900947980a7fdcc697d847d3bfb5a6998abe
Carlos Menco Guardo <84938811+carlosmencog@users.noreply.github.com>
2026-03-29 16:46:08 -0500

    Adding Blog pages

---
+++ src/content/config.ts
@@ -0,0 +1,11 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    body: z.string().optional(),
+  }),
+});
+
+export const collections = { blog };
